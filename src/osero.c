7df4da3f5ca06ed345df1473ab1db48a38474f45
takunoko <takunoko@takunoko-i7.(none)>
2012-10-15 17:18:36 +0900

    kansei

--- src/osero.c
+++ src/osero.c
@@ -16,279 +16,328 @@
 #include "osero.h"
 
 
+void osero_init(void){
+	
+	lcd_put_data(0,"Green turn      ");
+		
+	map[8][8]=map[7][7]=LED_GREEN;
+	led_plot(LED_GREEN,7,7);
+	led_plot(LED_GREEN,8,8);
+	map[8][7]=map[7][8]=LED_RED;
+	led_plot(LED_RED,7,8);
+	led_plot(LED_RED,8,7);
+
+	map[0][0]=LED_YELLOW;
+	led_plot(LED_YELLOW,0,0);
+
+	old_map = LED_NONE;
+
+}
+
 void osero_main(void){
 	
-	map[8][8]=map[7][7]=1;
-	led_plot(1,7,7);
-	led_plot(1,8,8);
-	map[8][7]=map[7][8]=2;
-	led_plot(2,7,8);
-	led_plot(2,8,7);
-
-	map[0][0]=3;
-	led_plot(3,0,0);
-
 	switch_state switch_state_p1;
 	switch_state_clear(&switch_state_p1);
+
+	switch_state switch_state_p0;
+	switch_state_clear(&switch_state_p0);
+
+	osero_init();
+
+	int hantei=0;
 
 	while(1){
 		idou=0;
 		put_check=0;
 
-		read_button(&switch_state_p1);
-		int hantei=0;
-		//自???????褐沚?
-		map_idou();
+		led_draw_line();
+		switch_get(SWITCH_CONT_P1,&switch_state_p1);
+		if((switch_state_p1.switch_u==1)&&(switch_state_p1.switch_prev_u==0)){
+			osero_map_idou_up();
+			lcd_put_data(1,"ue              ");
+		}else if((switch_state_p1.switch_d==1)&&(switch_state_p1.switch_prev_d==0)){
+			osero_map_idou_down();
+			lcd_put_data(1,"sita            ");
+		}else if((switch_state_p1.switch_l==1)&&(switch_state_p1.switch_prev_l==0)){
+			osero_map_idou_left();
+			lcd_put_data(1,"hidari          ");
+		}else if((switch_state_p1.switch_r==1)&&(switch_state_p1.switch_prev_r==0)){
+			osero_map_idou_right();
+			lcd_put_data(1,"migi            ");
+		}else if((switch_state_p1.switch_a==1)&&(switch_state_p1.switch_prev_a==0)){
+			osero_map_idou_put();
+			lcd_put_data(1,"put             ");
+		}else if((switch_state_p1.switch_b==1)&&(switch_state_p1.switch_prev_b==0)){
+			osero_map_idou_skip();
+			lcd_put_data(1,"skip            ");
+		}
+
+		switch_get(SWITCH_CONT_P0,&switch_state_p0);
+		if((switch_state_p0.switch_u==1)&&(switch_state_p0.switch_prev_u==0)){
+			osero_map_idou_up();
+			lcd_put_data(1,"ue              ");
+		}else if((switch_state_p0.switch_d==1)&&(switch_state_p0.switch_prev_d==0)){
+			osero_map_idou_down();
+			lcd_put_data(1,"sita            ");
+		}else if((switch_state_p0.switch_l==1)&&(switch_state_p0.switch_prev_l==0)){
+			osero_map_idou_left();
+			lcd_put_data(1,"hidari          ");
+		}else if((switch_state_p0.switch_r==1)&&(switch_state_p0.switch_prev_r==0)){
+			osero_map_idou_right();
+			lcd_put_data(1,"migi            ");
+		}else if((switch_state_p0.switch_a==1)&&(switch_state_p0.switch_prev_a==0)){
+			osero_map_idou_put();
+			lcd_put_data(1,"put             ");
+		}else if((switch_state_p0.switch_b==1)&&(switch_state_p0.switch_prev_b==0)){
+			osero_map_idou_skip();
+			lcd_put_data(1,"skip            ");
+		}
 
 		iro = (put_kaisuu%2+1);	
 	}
 }
 
-//??
-//
-void map_idou(void){
-	switch(idou){
-		case 2: {	//??????
-							map[map_y][map_x]=old_map;
-							led_plot(old_map,map_x,map_y);
-							if (map_y+1 < MAP_SIZE) {
-								old_map=map[map_y+1][map_x];
-								map[map_y+1][map_x]=3;
-								led_plot(3,map_x,map_y+1);
-								map_y++;
-							}else{
-								old_map=map[0][map_x];
-								map[0][map_x]=3;
-								led_plot(3,map_x,0);
-								map_y = 0;
-							}
-						}break;
-		case 8:	{	//??????
-							map[map_y][map_x]=old_map;
-							led_plot(old_map,map_x,map_y);
-							if (map_y-1 >= 0) {
-								old_map=map[map_y-1][map_x];
-								map[map_y-1][map_x]=3;
-								led_plot(3,map_x,map_y-1);
-								map_y--;
-							}else{
-								old_map=map[MAP_SIZE-1][map_x];
-								map[MAP_SIZE-1][map_x]=3;
-								led_plot(3,map_x,MAP_SIZE-1);
-								map_y = MAP_SIZE-1; 
-							}
-						}break;
-		case 6: {	//右????
-							map[map_y][map_x]=old_map;
-							led_plot(old_map,map_x,map_y);
-							if (map_x+1 < MAP_SIZE) {
-								old_map=map[map_y][map_x+1];
-								map[map_y][map_x+1]=3;
-								led_plot(3,map_x+1,map_y);
-								map_x++;
-							}else{
-								old_map=map[map_y-1][map_x];
-								map[map_y][0]=3;
-								led_plot(3,0,map_y);
-								map_x=0;
-							}
-						}break;
-		case 4:{	//??????
-						 map[map_y][map_x]=old_map;
-						 led_plot(old_map,map_x,map_y);	
-						 if (map_x-1 >= 0) {
-							 old_map=map[map_y][map_x-1];
-							 map[map_y][map_x-1]=3;
-							 led_plot(3,map_x-1,map_y);
-							 map_x--;
-						 }else{
-							 old_map=map[map_y][MAP_SIZE-1];
-							 map[map_y][MAP_SIZE-1]=3;
-							 led_plot(3,MAP_SIZE-1,map_y);
-							 map_x = MAP_SIZE-1;
-						 }
-					 }break;
-		case 5:{
-						 //??転????
-						 map_xx=map_xxx=map_x;
-						 map_yy=map_yyy=map_y;
-						 if(old_map != 0){
-							 break;
-						 }
-						 //??????	
-						 for (; map_yy < MAP_SIZE; map_yy++) {
-							 if(map[map_yy+1][map_xx] == 0 || map[map_yy+1][map_xx] == iro || map_yy+2 > MAP_SIZE){
-								 break;
-							 }
-							 if(map[map_yy+2][map_x] == iro){
-								 for (; map_yyy < map_yy+2; map_yyy++){
-									 map[map_yyy][map_x]=iro;
-									 led_plot(iro,map_x,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 map_xxx=map_xx=map_x;
-						 map_yyy=map_yy=map_y;
-						 //??????	
-						 for (; map_yy > 0 ; map_yy--) {
-							 if(map[map_yy-1][map_xx] == 0 || map[map_yy-1][map_xx] == iro || map_yy-2 < 0){
-								 break;
-							 }
-							 if(map[map_yy-2][map_x] == iro){
-								 for (; map_yyy > map_yy-2; map_yyy--){
-									 map[map_yyy][map_x]=iro;
-									 led_plot(iro,map_x,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 map_xxx=map_xx=map_x;
-						 map_yyy=map_yy=map_y;
-						 //右????	
-						 for (; map_xx < MAP_SIZE; map_xx++) {
-							 if(map[map_yy][map_xx+1] == 0 || map[map_yy][map_xx+1] == iro || map_xx+2 > MAP_SIZE){
-								 break;
-							 }
-							 if(map[map_y][map_xx+2] == iro){
-								 for (; map_xxx < map_xx+2; map_xxx++){
-									 map[map_y][map_xxx]=iro;
-									 led_plot(iro,map_x,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 map_xxx=map_xx=map_x;
-						 map_yyy=map_yy=map_y;
-						 //??????	
-						 for (; map_xx > 0; map_xx--) {
-							 if(map[map_yy][map_xx-1] == 0 || map[map_yy][map_xx-1] == iro || map_xx-2 < 0){
-								 break;
-							 }
-							 if(map[map_y][map_xx-2] == iro){
-								 for (; map_xxx > map_xx-2; map_xxx--){
-									 map[map_y][map_xxx]=iro;
-									 led_plot(iro,map_xxx,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 map_xxx=map_xx=map_x;
-						 map_yyy=map_yy=map_y;
-						 //右??????	
-						 for (; map_xx < MAP_SIZE && map_yy < MAP_SIZE; map_xx++,map_yy++) {
-							 if(map[map_yy+1][map_xx+1] == 0 || map[map_yy+1][map_xx+1] == iro || 
-									 map_yy+2 > MAP_SIZE || map_xx+2 > MAP_SIZE){
-								 break;
-							 }
-							 if(map[map_yy+2][map_xx+2] == iro){
-								 for (; map_xxx < map_xx+2 && map_yyy < map_yy+2; map_xxx++,map_yyy++){
-									 map[map_yyy][map_xxx]=iro;
-									 led_plot(iro,map_xxx,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 map_xxx=map_xx=map_x;
-						 map_yyy=map_yy=map_y;
-						 //????????	
-						 for (; map_xx > 0 && map_yy > 0; map_xx--,map_yy--) {
-							 if(map[map_yy-1][map_xx-1] == 0 || map[map_yy-1][map_xx-1] == iro ||
-									 map_yy-2 < 0 || map_xx-2 < 0){
-								 break;
-							 }
-							 if(map[map_yy-2][map_xx-2] == iro){
-								 for (; map_xxx > map_xx-2 && map_yyy > map_yy-2; map_xxx--,map_yyy--){
-									 map[map_yyy][map_xxx]=iro;
-									 led_plot(iro,map_xxx,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 map_xxx=map_xx=map_x;
-						 map_yyy=map_yy=map_y;
-						 //????????	
-						 for (; map_xx > 0 && map_yy < MAP_SIZE; map_xx--,map_yy++) {
-							 if(map[map_yy+1][map_xx-1] == 0 || map[map_yy+1][map_xx-1] == iro ||
-									 map_yy+2 > MAP_SIZE || map_xx-2 < 0){
-								 break;
-							 }
-							 if(map[map_yy+2][map_xx-2] == iro){
-								 for (; map_xxx > map_xx-2 && map_yyy < map_yy+2; map_xxx--,map_yyy++){
-									 map[map_yyy][map_xxx]=iro;
-									 led_plot(iro,map_xxx,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 map_xxx=map_xx=map_x;
-						 map_yyy=map_yy=map_y;
-						 //右??????	
-						 for (; map_xx < MAP_SIZE && map_yy > 0; map_xx++,map_yy--) {
-							 if(map[map_yy-1][map_xx+1] == 0 || map[map_yy-1][map_xx+1] == iro ||
-									 map_yy-2 < 0 || map_xx+2 > MAP_SIZE){
-								 break;
-							 }
-							 if(map[map_yy-2][map_xx+2] == iro){ 
-								 for (; map_xxx < map_xx+2 && map_yyy > map_yy-2; map_xxx++,map_yyy--){
-									 map[map_yyy][map_xxx]=iro;
-									 led_plot(iro,map_xxx,map_yyy);
-								 }
-								 put_check=1;
-							 }
-						 }
-						 if(put_check == 1){
-							 //置?????桙?????
-							 put_kaisuu++;
-							 kaisuu++;
-							 //黒・????????
-							 map[map_y][map_x] = iro;
-							 led_plot(iro,map_x,map_y);
-							 old_map = map[map_y][map_x];
-							 map[map_y][map_x] = 3;
-							 led_plot(iro,map_x,map_y);
-						 }
-					 }break;
-		case 99:{	put_kaisuu++;	}break;
-		default :{ }break;
+//マップをクリアー
+void osero_map_clear(void){
+	int x,y;
+	for(y=0;y<MAP_SIZE;y++){
+		for(x=0;x<MAP_SIZE;x++){
+			map[y][x] = LED_NONE;
+		}
 	}
 }
 
-void read_button(switch_state *switch_state_p1){
-	int chk;
-	chk = 0;
-	while(1){
-		led_draw_line();
-		switch_get(SWITCH_CONT_P1,switch_state_p1);
-		if((switch_state_p1->switch_u==1)&&(switch_state_p1->switch_prev_u==0)){
-			idou = 8;
-			lcd_put_data(1,"ue              ");
-			chk = 1;
-		}else if((switch_state_p1->switch_d==1)&&(switch_state_p1->switch_prev_d==0)){
-			idou = 2;
-			lcd_put_data(1,"sita            ");
-			chk = 1;
-		}else if((switch_state_p1->switch_l==1)&&(switch_state_p1->switch_prev_l==0)){
-			idou = 4;
-			lcd_put_data(1,"hidari          ");
-			chk = 1;
-		}else if((switch_state_p1->switch_r==1)&&(switch_state_p1->switch_prev_r==0)){
-			idou = 6;
-			lcd_put_data(1,"migi            ");
-			chk = 1;
-		}else if((switch_state_p1->switch_a==1)&&(switch_state_p1->switch_prev_a==0)){
-			idou = 5;
-			lcd_put_data(1,"put             ");
-			chk = 1;
-		}else if((switch_state_p1->switch_b==1)&&(switch_state_p1->switch_prev_b==0)){ 
-			idou = 99;
-			lcd_put_data(1,"skip            ");
-			chk = 1;
-		}
-		if(chk == 1){
-			sprintf(lcd_bf,"old_map=%d        ",old_map);
-			lcd_put_data(0,lcd_bf);
+//右移動イベントハンドラ
+void osero_map_idou_right(void){
+	map[map_y][map_x]=old_map;
+	led_plot(LED_NONE,map_x,map_y);
+	led_plot(old_map,map_x,map_y);
+	if (map_x+1 < MAP_SIZE) {
+		map_x++;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=LED_YELLOW;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}else{
+		map_x = 0;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=LED_YELLOW;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}
+}
+
+//左移動イベントハンドラ
+void osero_map_idou_left(void){
+	map[map_y][map_x]=old_map;
+	led_plot(LED_NONE,map_x,map_y);
+	led_plot(old_map,map_x,map_y);
+	if (map_x-1 >= 0) {
+		map_x--;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=LED_YELLOW;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}else{
+		map_x = MAP_SIZE-1;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=LED_YELLOW;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}
+}
+
+//上移動イベントハンドラ
+void osero_map_idou_up(void){
+	map[map_y][map_x]=old_map;
+	led_plot(LED_NONE,map_x,map_y);
+	led_plot(old_map,map_x,map_y);
+	if (map_y-1 >= 0) {
+		map_y--;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=LED_YELLOW;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}else{
+		map_y = MAP_SIZE-1;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=3;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}
+}
+
+//下移動イベントハンドラ
+void osero_map_idou_down(void){
+	map[map_y][map_x]=old_map;
+	led_plot(LED_NONE,map_x,map_y);
+	led_plot(old_map,map_x,map_y);
+	if (map_y+1 < MAP_SIZE) {
+		map_y++;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=LED_YELLOW;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}else{
+		map_y = 0;
+		old_map=map[map_y][map_x];
+		map[map_y][map_x]=3;
+		led_plot(LED_YELLOW,map_x,map_y);
+	}
+}
+
+//プットイベントハンドラ
+void osero_map_idou_put(void){
+	map_xx=map_xxx=map_x;
+	map_yy=map_yyy=map_y;
+	if(old_map != 0){
+		return;
+	}
+	//下反転
+	for (; map_yy < MAP_SIZE; map_yy++) {
+		if(map[map_yy+1][map_xx] == 0 || map[map_yy+1][map_xx] == iro || map_yy+2 > MAP_SIZE){
 			break;
 		}
+		if(map[map_yy+2][map_x] == iro){
+			for (; map_yyy < map_yy+2; map_yyy++){
+				map[map_yyy][map_x]=iro;
+				led_plot(LED_NONE,map_x,map_yyy);
+				led_plot(iro,map_x,map_yyy);
+			}
+			put_check=1;
+		}
 	}
+	map_xxx=map_xx=map_x;
+	map_yyy=map_yy=map_y;
+	//上判定
+	for (; map_yy > 0 ; map_yy--) {
+		if(map[map_yy-1][map_xx] == 0 || map[map_yy-1][map_xx] == iro || map_yy-2 < 0){
+			break;
+		}
+		if(map[map_yy-2][map_x] == iro){
+			for (; map_yyy > map_yy-2; map_yyy--){
+				map[map_yyy][map_x]=iro;
+				led_plot(LED_NONE,map_x,map_yyy);
+				led_plot(iro,map_x,map_yyy);
+			}
+			put_check=1;
+		}
+	}
+	map_xxx=map_xx=map_x;
+	map_yyy=map_yy=map_y;
+	//右判定
+	for (; map_xx < MAP_SIZE; map_xx++) {
+		if(map[map_yy][map_xx+1] == 0 || map[map_yy][map_xx+1] == iro || map_xx+2 > MAP_SIZE){
+			break;
+		}
+		if(map[map_y][map_xx+2] == iro){
+			for (; map_xxx < map_xx+2; map_xxx++){
+				map[map_y][map_xxx]=iro;
+				led_plot(LED_NONE,map_xxx,map_y);
+				led_plot(iro,map_xxx,map_y);
+			}
+			put_check=1;
+		}
+	}
+	map_xxx=map_xx=map_x;
+	map_yyy=map_yy=map_y;
+	//左判定
+	for (; map_xx > 0; map_xx--) {
+		if(map[map_yy][map_xx-1] == 0 || map[map_yy][map_xx-1] == iro || map_xx-2 < 0){
+			break;
+		}
+		if(map[map_y][map_xx-2] == iro){
+			for (; map_xxx > map_xx-2; map_xxx--){
+				map[map_y][map_xxx]=iro;
+				led_plot(LED_NONE,map_xxx,map_yyy);
+				led_plot(iro,map_xxx,map_yyy);
+			}
+			put_check=1;
+		}
+	}
+	map_xxx=map_xx=map_x;
+	map_yyy=map_yy=map_y;
+	//右上判定
+	for (; map_xx < MAP_SIZE && map_yy < MAP_SIZE; map_xx++,map_yy++) {
+		if(map[map_yy+1][map_xx+1] == 0 || map[map_yy+1][map_xx+1] == iro ||
+				map_yy+2 > MAP_SIZE || map_xx+2 > MAP_SIZE){
+			break;
+		}
+		if(map[map_yy+2][map_xx+2] == iro){
+			for (; map_xxx < map_xx+2 && map_yyy < map_yy+2; map_xxx++,map_yyy++){
+				map[map_yyy][map_xxx]=iro;
+				led_plot(LED_NONE,map_xxx,map_yyy);
+				led_plot(iro,map_xxx,map_yyy);
+			}
+			put_check=1;
+		}
+	}
+	map_xxx=map_xx=map_x;
+	map_yyy=map_yy=map_y;
+	//左下判定
+	for (; map_xx > 0 && map_yy > 0; map_xx--,map_yy--) {
+		if(map[map_yy-1][map_xx-1] == 0 || map[map_yy-1][map_xx-1] == iro ||
+				map_yy-2 < 0 || map_xx-2 < 0){
+			break;
+		}
+		if(map[map_yy-2][map_xx-2] == iro){
+			for (; map_xxx > map_xx-2 && map_yyy > map_yy-2; map_xxx--,map_yyy--){
+				map[map_yyy][map_xxx]=iro;
+				led_plot(LED_NONE,map_xxx,map_yyy);
+				led_plot(iro,map_xxx,map_yyy);
+			}
+			put_check=1;
+		}
+	}
+	map_xxx=map_xx=map_x;
+	map_yyy=map_yy=map_y;
+	//左上判定
+	for (; map_xx > 0 && map_yy < MAP_SIZE; map_xx--,map_yy++) {
+		if(map[map_yy+1][map_xx-1] == 0 || map[map_yy+1][map_xx-1] == iro ||
+				map_yy+2 > MAP_SIZE || map_xx-2 < 0){
+			break;
+		}
+		if(map[map_yy+2][map_xx-2] == iro){
+			for (; map_xxx > map_xx-2 && map_yyy < map_yy+2; map_xxx--,map_yyy++){
+				map[map_yyy][map_xxx]=iro;
+				led_plot(LED_NONE,map_xxx,map_yyy);
+				led_plot(iro,map_xxx,map_yyy);
+			}
+			put_check=1;
+		}
+	}
+	map_xxx=map_xx=map_x;
+	map_yyy=map_yy=map_y;
+	//右上判定
+	for (; map_xx < MAP_SIZE && map_yy > 0; map_xx++,map_yy--) {
+		if(map[map_yy-1][map_xx+1] == 0 || map[map_yy-1][map_xx+1] == iro ||
+				map_yy-2 < 0 || map_xx+2 > MAP_SIZE){
+			break;
+		}
+		if(map[map_yy-2][map_xx+2] == iro){
+			for (; map_xxx < map_xx+2 && map_yyy > map_yy-2; map_xxx++,map_yyy--){
+				map[map_yyy][map_xxx]=iro;
+				led_plot(LED_NONE,map_xxx,map_yyy);
+				led_plot(iro,map_xxx,map_yyy);
+			}
+			put_check=1;
+		}
+	}
+	if(put_check == 1){
+		//置いた回数
+		put_kaisuu++;
+		kaisuu++;
+		if (kaisuu%2) {
+			lcd_put_data(0,"Green turn      ");
+		}else{
+			lcd_put_data(0,"Red turn        ");
+		}
+		
+		map[map_y][map_x] = iro;
+		led_plot(LED_NONE,map_x,map_y);
+		led_plot(iro,map_x,map_y);
+		old_map = map[map_y][map_x];
+
+		map[map_y][map_x] = 3;
+		led_plot(LED_NONE,map_x,map_y);
+		led_plot(iro,map_x,map_y);
+	}
+}
+
+//スキップイベントハンドラ
+void osero_map_idou_skip(void){
+	put_kaisuu++;
 }
--- src/osero.h
+++ src/osero.h
@@ -11,13 +11,22 @@ int map_xx,map_yy,map_xxx,map_yyy;
 int put_check;
 int old_map=0;
 int kaisuu=1;
-char lcd_bf[50];
+char lcd_bf[17];
 
-void osero(void);
-void hyouji(void);
 void map_idou(void);
 void read_button(switch_state*);
+
+//メイン及び初期化関数
 void osero_main(void);
+void osero_init(void);
+void osero_map_clear(void);
+
+//移動用のイベントハンドラ
+void osero_map_idou_right(void);
+void osero_map_idou_left(void);
+void osero_map_idou_up(void);
+void osero_map_idou_down(void);
+void osero_map_idou_put(void);
+void osero_map_idou_skip(void);
 
 #endif /* SWITCH_H_ */
-
